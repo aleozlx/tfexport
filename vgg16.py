b65660e4a47b1e99fd606cf6c7848ee532ea39b3
Alex Yang <zy5f9@mail.missouri.edu>
2019-07-15 15:50:30 -0500

    init

---
+++ vgg16.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import tensorflow as tf
+import freeze_graph
+tf.keras.backend.clear_session()
+
+with tf.variable_scope('DataSource'):
+    images = tf.placeholder(tf.uint8, (1,256,256,3), 'input')
+    images = tf.image.per_image_standardization(images)
+
+with tf.variable_scope('DCNN'):
+    dcnn = tf.keras.applications.VGG16(
+        input_tensor=images,
+        include_top=False,
+        weights=None,
+    )
+
+OUTPUT = Path('/tmp')
+FNAME = 'vgg16'
+
+with tf.Session() as sess:
+    tf.keras.backend.set_session(sess)
+    with tf.variable_scope('Initializer'):
+        dcnn.load_weights('/tank/datasets/research/model_weights/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5')
+    tf.io.write_graph(sess.graph_def, str(OUTPUT), f'{FNAME}.pbtxt', as_text=True)
+    tf.io.write_graph(sess.graph_def, str(OUTPUT), f'{FNAME}.pb', as_text=False)
+    saver = tf.train.Saver(tf.trainable_variables())
+    saver.save(sess, str(OUTPUT / FNAME))
+
+freeze_graph.freeze_graph(
+    str(OUTPUT / f'{FNAME}.pb'), input_saver="", input_binary=True,
+    input_checkpoint=str(OUTPUT / f'{FNAME}'),
+    output_node_names='DCNN/block5_pool/MaxPool',
+    initializer_nodes='',
+    restore_op_name='', # Unused
+    filename_tensor_name='', # Unused
+    output_graph=str(OUTPUT / f'{FNAME}-frozen.pb'),
+    clear_devices=True
+)
